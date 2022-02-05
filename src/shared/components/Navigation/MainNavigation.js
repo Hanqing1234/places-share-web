80f6245d53eaadaf4b3bd0c54da37beef2a72d7a
Hanqing Liu <lhq3361951@gmail.com>
2022-02-04 21:33:06 -0500

    added side drawer component

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,26 +1,35 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import "./MainHeader.css";
 
 import MainHeader from "./MainHeader";
-import NavLinks from './NavLinks';
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your places</Link>
-      </h1>
-      <nav>
-      <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,9 @@
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => {
+  return (
+      <aside className="side-drawer">{props.children}</aside>
+    )
+};
+
+export default SideDrawer;
