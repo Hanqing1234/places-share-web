f62dec0375be5577e6d2a2fb43200b3a843e9a37
Hanqing Liu <lhq3361951@gmail.com>
2022-02-06 16:09:50 -0500

    add a map component and render google map in modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 import "./PlaceItem.css";
 
@@ -28,7 +29,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!</h2>
+          <Map center={props.coordinates} zoom={16}/>
         </div>
       </Modal>
       <li className="place-item">
