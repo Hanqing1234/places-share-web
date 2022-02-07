215500be2855a65689033c8a185f7fc8dc9b48a6
Hanqing Liu <lhq3361951@gmail.com>
2022-02-06 23:42:09 -0500

    added input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,19 @@
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" validators={[]} onChange={[]} />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        errorText="Please enter a valid title"
+        validators={[VALIDATOR_REQUIRE()]}
+        onChange={[]}
+      />
     </form>
   );
 };
