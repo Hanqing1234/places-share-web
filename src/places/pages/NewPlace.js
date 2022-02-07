3eb075971b6cc1748fac414ab0b0a80aca08a4e7
Hanqing Liu <lhq3361951@gmail.com>
2022-02-06 22:41:21 -0500

    added a coutome form input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,13 @@
+import "./NewPlace.css";
+
+import Input from "../../shared/components/FormElements/Input";
+
 const NewPlace = () => {
-    return <h2>New Place Works</h2>
-}
+  return (
+    <form className="place-form">
+      <Input type="text" label="Title" element="input" validators={[]} onChange={[]} />
+    </form>
+  );
+};
 
 export default NewPlace;
