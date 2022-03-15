b7c04d1206ad15b02a208a31089d8437755b345a
Hanqing Liu <lhq3361951@gmail.com>
2022-03-15 16:22:09 -0400

    preparation for production

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,13 +6,20 @@ import {
   Switch,
 } from "react-router-dom";
 
-import User from "./user/pages/User";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
+//import Users from "./user/pages/Users";
+//import NewPlace from "./places/pages/NewPlace";
+//import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
+//import UpdatePlace from "./places/pages/UpdatePlace";
+//import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+
+const Users = React.lazy(() => import("./user/pages/User"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -34,7 +41,7 @@ const App = () => {
     routes = (
       <Switch>
         <Route path="/" exact>
-          <User />
+          <Users />
         </Route>
         <Route path="/:userId/places" exact>
           <UserPlaces />
@@ -52,7 +59,7 @@ const App = () => {
     routes = (
       <Switch>
         <Route path="/" exact>
-          <User />
+          <Users />
         </Route>
         <Route path="/:userId/places" exact>
           <UserPlaces />
@@ -67,12 +74,25 @@ const App = () => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, userId: userId, login: login, logout: logout }}
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
     >
       <Router>
         <MainNavigation />
         <main>
-          {routes}
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
